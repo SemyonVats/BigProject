8a855b6b2c21b8b79ef382a816dde29bce7eef31
SemyonVats <123807039+SemyonVats@users.noreply.github.com>
2024-04-24 14:02:14 +0300

    Update1 main.py

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ words2 = "Мы поздравляем с Днём Рождения:"
 print("Сколько людей мы сегодня поздравляем?")
 ch = int(input())
 print("Кого сегодня поздравляем?")
-print("Введите имя и фамилию в ВИНИТЕЛЬНОМ падеже и класс с буквой. Класс с буквой отделяйте переводом строки.")
+print("Введите имя и фамилию в ВИНИТЕЛЬНОМ падеже и класс с буквой.")
+print("Класс с буквой ученика или фамилию преподавателя отделяйте переводом строки.")
 print("Данные разных людей разделяйте переводом строки.")
 
 #Объявление переменных
-our_fon = Image.open('Фон.jpg')
+our_fon1 = Image.open('Фон1.jpg')
 tabl = Image.open('Табл.jpg')
 tort1 = Image.open('Торт1.jpg')
 tort2 = Image.open('Торт2.jpg')
@@ -21,32 +22,38 @@ tort3 = Image.open('Торт3.jpg')
 podarok1 = Image.open('Подарок1.jpg')
 podarok2 = Image.open('Подарок2.jpg')
 podarok3 = Image.open('Подарок3.jpg')
-font1 = ImageFont.truetype("arial.ttf", 70)
+font1 = ImageFont.truetype("arial.ttf", 65)
 font2 = ImageFont.truetype("calibri.ttf", 35)
 mask = [tort1, tort2, tort3, podarok1, podarok2, podarok3]
 
+#Случайный выбор фона
+fonmask = [our_fon1]
+our_fon = random.choice(fonmask)
+fonmask.remove(our_fon)
 
-if(ch == 1):
+# Вывод текста на экран с учётом шрифта и даты
+date = ImageDraw.Draw(our_fon1)
+date.text((700, 80), words1, (0, 0, 0), anchor="ms", font=font1)
+date.text((700, 150), words2, (0, 0, 0), anchor="ms", font=font1)
+
+if ch == 1:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
-
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
 
     #Вывод именинников
     our_fon.paste(tabl, (500, 300))
-    date.text((500, 320), people1, (0, 0, 0), font=font2)
-    date.text((660, 360), class1, (0, 0, 0), font=font2)
+    date.text((725, 350), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((725, 410), class1, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
     our_fon.paste(present1, (200, 350))
     mask.remove(present1)
     present2 = random.choice(mask)
-    our_fon.paste(present2 , (1000, 450))
+    our_fon.paste(present2, (1000, 450))
     mask.remove(present2)
     present3 = random.choice(mask)
     our_fon.paste(present3, (500, 550))
@@ -56,26 +63,25 @@ if(ch == 1):
     our_fon.show()
     our_fon.save("Картинка1.jpg")
 
-if(ch == 2):
+if ch == 2:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
     people2 = input()
     class2 = input()
-    class2 = class2 + " класс"
-
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class2)):
+        if (class2[i] >= '0' and class2[i] <= '9'):
+            class2 = class2 + " класс"
 
     #Вывод именинников
     our_fon.paste(tabl, (200, 250))
-    date.text((200, 270), people1, (0, 0, 0), font=font2)
-    date.text((360, 310), class1, (0, 0, 0), font=font2)
+    date.text((425, 300), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((425, 360), class1, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (900, 700))
-    date.text((900, 720), people2, (0, 0, 0), font=font2)
-    date.text((1060, 760), class2, (0, 0, 0), font=font2)
+    date.text((1125, 750), people2, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1125, 810), class2, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
@@ -94,32 +100,33 @@ if(ch == 2):
 
 
 
-if(ch == 3):
+if ch == 3:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
     people2 = input()
     class2 = input()
-    class2 = class2 + " класс"
+    for i in range(0, len(class2)):
+        if (class2[i] >= '0' and class2[i] <= '9'):
+            class2 = class2 + " класс"
     people3 = input()
     class3 = input()
-    class3 = class3 + " класс"
-
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class3)):
+        if (class3[i] >= '0' and class3[i] <= '9'):
+            class3 = class3 + " класс"
 
     #Вывод именинников
     our_fon.paste(tabl, (200, 250))
-    date.text((200, 270), people1, (0, 0, 0), font=font2)
-    date.text((360, 310), class1, (0, 0, 0), font=font2)
+    date.text((425, 300), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((425, 360), class1, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (900, 700))
-    date.text((900, 720), people2, (0, 0, 0), font=font2)
-    date.text((1060, 760), class2, (0, 0, 0), font=font2)
+    date.text((1125, 750), people2, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1125, 810), class2, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (550, 475))
-    date.text((550, 495), people3, (0, 0, 0), font=font2)
-    date.text((710, 535), class3, (0, 0, 0), font=font2)
+    date.text((775, 525), people3, (0, 0, 0), anchor="ms", font=font2)
+    date.text((775, 585), class3, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
@@ -139,37 +146,41 @@ if(ch == 3):
     our_fon.show()
     our_fon.save("Картинка1.jpg")
 
-if(ch == 4):
+if ch == 4:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
     people2 = input()
     class2 = input()
-    class2 = class2 + " класс"
+    for i in range(0, len(class2)):
+        if (class2[i] >= '0' and class2[i] <= '9'):
+            class2 = class2 + " класс"
     people3 = input()
     class3 = input()
-    class3 = class3 + " класс"
+    for i in range(0, len(class3)):
+        if (class3[i] >= '0' and class3[i] <= '9'):
+            class3 = class3 + " класс"
     people4 = input()
     class4 = input()
-    class4 = class4 + " класс"
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class4)):
+        if (class4[i] >= '0' and class4[i] <= '9'):
+            class4 = class4 + " класс"
 
     #Вывод именинников
     our_fon.paste(tabl, (10, 200))
-    date.text((10, 220), people1, (0, 0, 0), font=font2)
-    date.text((170, 260), class1, (0, 0, 0), font=font2)
+    date.text((235, 250), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((235, 310), class1, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (700, 800))
-    date.text((700, 820), people2, (0, 0, 0), font=font2)
-    date.text((860, 860), class2, (0, 0, 0), font=font2)
+    date.text((925, 850), people2, (0, 0, 0), anchor="ms", font=font2)
+    date.text((925, 910), class2, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (900, 200))
-    date.text((900, 220), people3, (0, 0, 0), font=font2)
-    date.text((1060, 260), class3, (0, 0, 0), font=font2)
+    date.text((1125, 250), people3, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1125, 310), class3, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (450, 575))
-    date.text((450, 595), people4, (0, 0, 0), font=font2)
-    date.text((610, 635), class4, (0, 0, 0), font=font2)
+    date.text((675, 625), people4, (0, 0, 0), anchor="ms", font=font2)
+    date.text((675, 685), class4, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
@@ -189,43 +200,49 @@ if(ch == 4):
     our_fon.show()
     our_fon.save("Картинка1.jpg")
 
-if(ch == 5):
+if ch == 5:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
     people2 = input()
     class2 = input()
-    class2 = class2 + " класс"
+    for i in range(0, len(class2)):
+        if (class2[i] >= '0' and class2[i] <= '9'):
+            class2 = class2 + " класс"
     people3 = input()
     class3 = input()
-    class3 = class3 + " класс"
+    for i in range(0, len(class3)):
+        if (class3[i] >= '0' and class3[i] <= '9'):
+            class3 = class3 + " класс"
     people4 = input()
     class4 = input()
-    class4 = class4 + " класс"
+    for i in range(0, len(class4)):
+        if (class4[i] >= '0' and class4[i] <= '9'):
+            class4 = class4 + " класс"
     people5 = input()
     class5 = input()
-    class5 = class5 + " класс"
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class5)):
+        if (class5[i] >= '0' and class5[i] <= '9'):
+            class5 = class5 + " класс"
 
     #Вывод именинников
     our_fon.paste(tabl, (10, 200))
-    date.text((10, 220), people1, (0, 0, 0), font=font2)
-    date.text((170, 260), class1, (0, 0, 0), font=font2)
+    date.text((235, 250), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((235, 310), class1, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (900, 200))
-    date.text((900, 220), people3, (0, 0, 0), font=font2)
-    date.text((1060, 260), class3, (0, 0, 0), font=font2)
+    date.text((1125, 250), people3, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1125, 310), class3, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (10, 650))
-    date.text((10, 670), people2, (0, 0, 0), font=font2)
-    date.text((170, 720), class2, (0, 0, 0), font=font2)
+    date.text((235, 700), people2, (0, 0, 0), anchor="ms", font=font2)
+    date.text((235, 760), class2, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (830, 740))
-    date.text((830, 760), people4, (0, 0, 0), font=font2)
-    date.text((990, 800), class4, (0, 0, 0), font=font2)
+    date.text((1055, 790), people4, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1055, 850), class4, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (460, 875))
-    date.text((460, 895), people5, (0, 0, 0), font=font2)
-    date.text((620, 935), class5, (0, 0, 0), font=font2)
+    date.text((685, 925), people5, (0, 0, 0), anchor="ms", font=font2)
+    date.text((685, 985), class5, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
@@ -248,49 +265,57 @@ if(ch == 5):
     our_fon.show()
     our_fon.save("Картинка1.jpg")
 
-if(ch == 6):
+if ch == 6:
     people1 = input()
     class1 = input()
-    class1 = class1 + " класс"
+    for i in range(0, len(class1)):
+        if (class1[i] >= '0' and class1[i] <= '9'):
+            class1 = class1 + " класс"
     people2 = input()
     class2 = input()
-    class2 = class2 + " класс"
+    for i in range(0, len(class2)):
+        if (class2[i] >= '0' and class2[i] <= '9'):
+            class2 = class2 + " класс"
     people3 = input()
     class3 = input()
-    class3 = class3 + " класс"
+    for i in range(0, len(class3)):
+        if (class3[i] >= '0' and class3[i] <= '9'):
+            class3 = class3 + " класс"
     people4 = input()
     class4 = input()
-    class4 = class4 + " класс"
+    for i in range(0, len(class4)):
+        if (class4[i] >= '0' and class4[i] <= '9'):
+            class4 = class4 + " класс"
     people5 = input()
     class5 = input()
-    class5 = class5 + " класс"
+    for i in range(0, len(class5)):
+        if (class5[i] >= '0' and class5[i] <= '9'):
+            class5 = class5 + " класс"
     people6 = input()
     class6 = input()
-    class6 = class6 + " класс"
-    # Вывод текста на экран с учётом шрифта и даты
-    date = ImageDraw.Draw(our_fon)
-    date.text((400, 10), words1, (0, 0, 0), font=font1)
-    date.text((130, 100), words2, (0, 0, 0), font=font1)
+    for i in range(0, len(class6)):
+        if (class6[i] >= '0' and class6[i] <= '9'):
+            class6 = class6 + " класс"
 
     # Вывод именинников
     our_fon.paste(tabl, (10, 200))
-    date.text((10, 220), people1, (0, 0, 0), font=font2)
-    date.text((170, 260), class1, (0, 0, 0), font=font2)
+    date.text((235, 250), people1, (0, 0, 0), anchor="ms", font=font2)
+    date.text((235, 310), class1, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (900, 200))
-    date.text((900, 220), people3, (0, 0, 0), font=font2)
-    date.text((1060, 260), class3, (0, 0, 0), font=font2)
+    date.text((1125, 250), people3, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1125, 310), class3, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (10, 650))
-    date.text((10, 670), people2, (0, 0, 0), font=font2)
-    date.text((170, 720), class2, (0, 0, 0), font=font2)
+    date.text((235, 700), people2, (0, 0, 0), anchor="ms", font=font2)
+    date.text((235, 760), class2, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (830, 740))
-    date.text((830, 760), people4, (0, 0, 0), font=font2)
-    date.text((990, 800), class4, (0, 0, 0), font=font2)
-    our_fon.paste(tabl, (460, 875))
-    date.text((460, 895), people5, (0, 0, 0), font=font2)
-    date.text((620, 935), class5, (0, 0, 0), font=font2)
+    date.text((1055, 790), people4, (0, 0, 0), anchor="ms", font=font2)
+    date.text((1055, 850), class4, (0, 0, 0), anchor="ms", font=font2)
+    our_fon.paste(tabl, (360, 875))
+    date.text((585, 925), people5, (0, 0, 0), anchor="ms", font=font2)
+    date.text((585, 985), class5, (0, 0, 0), anchor="ms", font=font2)
     our_fon.paste(tabl, (650, 470))
-    date.text((650, 490), people6, (0, 0, 0), font=font2)
-    date.text((810, 530), class6, (0, 0, 0), font=font2)
+    date.text((875, 520), people6, (0, 0, 0), anchor="ms", font=font2)
+    date.text((875, 580), class6, (0, 0, 0), anchor="ms", font=font2)
 
     #Вывод подарков для именинников
     present1 = random.choice(mask)
@@ -309,3 +334,4 @@ if(ch == 6):
     #Экран
     our_fon.show()
     our_fon.save("Картинка1.jpg")
+
